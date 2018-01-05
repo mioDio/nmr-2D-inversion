c80412e9eb2bf442643e639b30ab75dcb756115c
Hertel <stefan.a.hertel@gmail.com>
2018-01-05 09:55:28 -0600

    Some cosmetic changes to example distributions.

--- example_distribution.py
+++ example_distribution.py
@@ -1,16 +1,11 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Wed Jun 14 20:23:44 2017
-
-@author: Stefan Hertel
-"""
 import numpy as np
 from scipy.ndimage.filters import gaussian_filter
 
 '''
 - can be imported by algorithms for 2d inversion as an input distribution
 - define a density function e.g. f(T1,T2) = Gaussian peak + ridge
-- e.g. dim(f(T1,T2)) = 100x100
+- e.g. dim(F(T1,T2)) = 100x100
 '''
 
 #def centered_gaussian(sigma, Nx, Ny):
@@ -29,10 +24,8 @@ def gaussian(sigma, T1, T2, tau1, tau2):
 def single_gauss(Nx=100, Ny=100, T1=0.7, T2=0.2, sigmaGauss=3.0):
     T1min = 1e-3
     T1max = 10.0
-
     T2min = 1e-3
     T2max = 10.0
-
     T1Arr = np.logspace(np.log10(T1min),
                         np.log10(T1max),
                         Nx, endpoint=True,
@@ -42,16 +35,10 @@ def single_gauss(Nx=100, Ny=100, T1=0.7, T2=0.2, sigmaGauss=3.0):
                         Ny,
                         endpoint=True,
                         base=10.0)
-
-#    T1 = 0.7  # seconds #T1Arr[50]
-#    T2 = 0.2  # seconds #T2Arr[40]
-
     print("T1 = %.2f s" % T1)
     print("T2 = %.2f s" % T2)
-
     T1_index = np.where(T1Arr > T1)[0][0]
     T2_index = np.where(T2Arr > T2)[0][0]
-
     trueF = np.zeros([Nx, Ny])
     for i in range(Nx):
         for j in range(Ny):
@@ -61,13 +48,10 @@ def single_gauss(Nx=100, Ny=100, T1=0.7, T2=0.2, sigmaGauss=3.0):
 
 def ridge_gauss(Nx=100, Ny=100, sigmaGauss=3.0, sigmaRidge=2):
     # sigmas are given in pixels
-
     T1min = 1e-3
     T1max = 10.0
-
     T2min = 1e-3
     T2max = 10.0
-
     T1Arr = np.logspace(np.log10(T1min),
                         np.log10(T1max),
                         Nx, endpoint=True,
@@ -77,16 +61,12 @@ def ridge_gauss(Nx=100, Ny=100, sigmaGauss=3.0, sigmaRidge=2):
                         Ny,
                         endpoint=True,
                         base=10.0)
-
     T1 = 0.7  # seconds #T1Arr[50]
     T2 = 0.2  # seconds #T2Arr[40]
-
     print("T1 = %.2f s" % T1)
     print("T2 = %.2f s" % T2)
-
     T1_index = np.where(T1Arr > T1)[0][0]
     T2_index = np.where(T2Arr > T2)[0][0]
-
     trueF = np.zeros([Nx, Ny])
     for i in range(Nx):
         for j in range(Ny):
@@ -95,15 +75,12 @@ def ridge_gauss(Nx=100, Ny=100, sigmaGauss=3.0, sigmaRidge=2):
     xLower = 17
     m = 0.5
     n = 30
-
     F = np.zeros([Nx, Ny])
     for i in range(xLower, xUpper, 1):
         j = int(m * i + n)
         F[i, j] = 1.0
-
     Ridge = gaussian_filter(F, sigma=sigmaRidge)
     Ridge = Ridge/max(Ridge.ravel())
-
     trueF = trueF/max(trueF.ravel())
     trueF = trueF + Ridge.T
     trueF = trueF/np.sum(trueF.ravel())
@@ -118,7 +95,6 @@ if __name__ == "__main__":
                                       sigmaRidge=2)
     fig = plt.figure()
     ax = fig.add_axes([.1, .1, .8, .8])
-
     ax.pcolor(T2Arr,
               T1Arr,
               trueF,
